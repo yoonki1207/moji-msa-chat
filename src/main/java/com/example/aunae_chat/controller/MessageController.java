7ad729572e92a3b592f681b8fe9509c895ddba4e
yoonki1207 <yoonki1207@gmail.com>
2023-11-13 16:50:58 +0900

    Feat: set sender with session

--- src/main/java/com/example/aunae_chat/controller/MessageController.java
+++ src/main/java/com/example/aunae_chat/controller/MessageController.java
@@ -6,6 +6,7 @@ import com.example.aunae_chat.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +26,19 @@ public class MessageController {
      * @param messageDto Message DTO
      */
     @MessageMapping("/chat/message")
-    public void enter(ChatMessageDto messageDto) {
+    public void enter(SimpMessageHeaderAccessor accessor, ChatMessageDto messageDto) {
+        Long senderId = (Long) accessor.getSessionAttributes().get("id");
+        String senderName = (String) accessor.getSessionAttributes().get("name");
+
         log.info("Message: {}", messageDto.toString());
+
+        if(senderId == null || senderName == null) {
+            return;
+        }
+
+        // set sender data with session
+        messageDto.setSenderId(senderId);
+        messageDto.setSender(senderName);
 
         // TODO: 방 확인 후 존재하면 입장 메시지 전송, else 무시
         if (ChatMessageDto.MessageType.ENTER.equals(messageDto.getMessageType())) {
