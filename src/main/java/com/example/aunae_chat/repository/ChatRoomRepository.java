005c90d356ff8ad44bb694c2f7378405e146b7a3
yoonki1207 <yoonki1207@gmail.com>
2025-05-15 22:55:22 +0900

    Feat: user가 미참여한 채팅방 리스트 repo

--- src/main/java/com/example/aunae_chat/repository/ChatRoomRepository.java
+++ src/main/java/com/example/aunae_chat/repository/ChatRoomRepository.java
@@ -2,6 +2,7 @@ package com.example.aunae_chat.repository;
 
 import com.example.aunae_chat.data.documents.ChatRoom;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,8 @@ public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
     Optional<ChatRoom> findByChatRoomId(Long chatRoomId);
     Optional<ChatRoom> findByBungaeId(Long bungaeId);
     List<ChatRoom> findByUsers_UserId(Long userId);
+
+    // user가 미참여한 채팅방 리스트
+    @Query("{ 'users':  { $not : { $elemMatch:  {'userId': ?0}}}}")
+    List<ChatRoom> findAllByUserIdNotPresent(Long userId);
 }
