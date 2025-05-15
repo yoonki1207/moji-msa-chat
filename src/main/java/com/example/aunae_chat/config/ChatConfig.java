0e19d85c536cc5feca436715333cd31106030511
yoonki1207 <yoonki1207@gmail.com>
2025-05-15 22:58:25 +0900

    Feat: ChatConfig 인터프리터, 인바운드 추가 - ws에 WebSocketHandShakeInterceptor 인터셉터 추가 - Client Inbound 채널에 BearerAuthInterceptor 추가

--- src/main/java/com/example/aunae_chat/config/ChatConfig.java
+++ src/main/java/com/example/aunae_chat/config/ChatConfig.java
@@ -1,6 +1,9 @@
 package com.example.aunae_chat.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -10,9 +13,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
+    private static final Logger log = LoggerFactory.getLogger(ChatConfig.class);
+    final private WebSocketHandShakeInterceptor webSocketHandShakeInterceptor;
+    final private AuthorizationExtractor authorizationExtractor;
+
+    public ChatConfig(WebSocketHandShakeInterceptor webSocketHandShakeInterceptor, AuthorizationExtractor authorizationExtractor) {
+        this.webSocketHandShakeInterceptor = webSocketHandShakeInterceptor;
+        this.authorizationExtractor = authorizationExtractor;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
+                .addInterceptors(webSocketHandShakeInterceptor)
 //                .setAllowedOrigins("http://localhost:3000")
                 .setAllowedOriginPatterns("*").withSockJS().setSuppressCors(true);
     }
@@ -21,5 +34,12 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/queue", "/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        WebSocketMessageBrokerConfigurer.super.configureClientInboundChannel(registration);
+        log.info("Registering client inbound channel");
+        registration.interceptors(new BearerAuthInterceptor(authorizationExtractor));
     }
 }
